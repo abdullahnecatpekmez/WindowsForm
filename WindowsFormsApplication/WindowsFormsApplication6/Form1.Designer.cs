2ff1c6ed96e4302f43c8bef17ae55478441725ec
abdullahnecatpekmez <abdullah_pekmez89@hotmail.com>
2015-07-21 18:31:03 +0300

    WindowsForm
    C#

---
+++ WindowsFormsApplication/WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addRadioButton = new System.Windows.Forms.RadioButton();
+            this.translateRadioButton = new System.Windows.Forms.RadioButton();
+            this.deleteRadioButton = new System.Windows.Forms.RadioButton();
+            this.cizimAlaniLabel = new System.Windows.Forms.Label();
+            this.clusterButton = new System.Windows.Forms.Button();
+            this.clusterNumberTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addRadioButton
+            // 
+            this.addRadioButton.AutoSize = true;
+            this.addRadioButton.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.addRadioButton.Location = new System.Drawing.Point(65, 111);
+            this.addRadioButton.Name = "addRadioButton";
+            this.addRadioButton.Size = new System.Drawing.Size(103, 22);
+            this.addRadioButton.TabIndex = 0;
+            this.addRadioButton.TabStop = true;
+            this.addRadioButton.Text = "Nokta Ekle";
+            this.addRadioButton.UseVisualStyleBackColor = true;
+            this.addRadioButton.CheckedChanged += new System.EventHandler(this.addRadioButton_CheckedChanged);
+            // 
+            // translateRadioButton
+            // 
+            this.translateRadioButton.AutoSize = true;
+            this.translateRadioButton.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.translateRadioButton.Location = new System.Drawing.Point(65, 138);
+            this.translateRadioButton.Name = "translateRadioButton";
+            this.translateRadioButton.Size = new System.Drawing.Size(108, 22);
+            this.translateRadioButton.TabIndex = 1;
+            this.translateRadioButton.TabStop = true;
+            this.translateRadioButton.Text = "Yer Değiştir";
+            this.translateRadioButton.UseVisualStyleBackColor = true;
+            this.translateRadioButton.CheckedChanged += new System.EventHandler(this.translateRadioButton_CheckedChanged);
+            // 
+            // deleteRadioButton
+            // 
+            this.deleteRadioButton.AutoSize = true;
+            this.deleteRadioButton.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.deleteRadioButton.Location = new System.Drawing.Point(65, 165);
+            this.deleteRadioButton.Name = "deleteRadioButton";
+            this.deleteRadioButton.Size = new System.Drawing.Size(90, 22);
+            this.deleteRadioButton.TabIndex = 2;
+            this.deleteRadioButton.TabStop = true;
+            this.deleteRadioButton.Text = "Nokta Sil";
+            this.deleteRadioButton.UseVisualStyleBackColor = true;
+            this.deleteRadioButton.CheckedChanged += new System.EventHandler(this.deleteRadioButton_CheckedChanged);
+            // 
+            // cizimAlaniLabel
+            // 
+            this.cizimAlaniLabel.AutoSize = true;
+            this.cizimAlaniLabel.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cizimAlaniLabel.Location = new System.Drawing.Point(335, 45);
+            this.cizimAlaniLabel.Name = "cizimAlaniLabel";
+            this.cizimAlaniLabel.Size = new System.Drawing.Size(129, 24);
+            this.cizimAlaniLabel.TabIndex = 3;
+            this.cizimAlaniLabel.Text = "Çizim Alanı";
+            // 
+            // clusterButton
+            // 
+            this.clusterButton.Location = new System.Drawing.Point(6, 49);
+            this.clusterButton.Name = "clusterButton";
+            this.clusterButton.Size = new System.Drawing.Size(84, 34);
+            this.clusterButton.TabIndex = 4;
+            this.clusterButton.Text = "Kümele";
+            this.clusterButton.UseVisualStyleBackColor = true;
+            this.clusterButton.Click += new System.EventHandler(this.clusterButton_Click);
+            // 
+            // clusterNumberTextBox
+            // 
+            this.clusterNumberTextBox.Location = new System.Drawing.Point(6, 21);
+            this.clusterNumberTextBox.Multiline = true;
+            this.clusterNumberTextBox.Name = "clusterNumberTextBox";
+            this.clusterNumberTextBox.Size = new System.Drawing.Size(84, 22);
+            this.clusterNumberTextBox.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.clusterNumberTextBox);
+            this.groupBox1.Controls.Add(this.clusterButton);
+            this.groupBox1.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(65, 224);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(108, 89);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Küme Sayısı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 454);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cizimAlaniLabel);
+            this.Controls.Add(this.deleteRadioButton);
+            this.Controls.Add(this.translateRadioButton);
+            this.Controls.Add(this.addRadioButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton addRadioButton;
+        private System.Windows.Forms.RadioButton translateRadioButton;
+        private System.Windows.Forms.RadioButton deleteRadioButton;
+        private System.Windows.Forms.Label cizimAlaniLabel;
+        private System.Windows.Forms.Button clusterButton;
+        private System.Windows.Forms.TextBox clusterNumberTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+
+    }
+}
+
---
+++ WindowsFormsApplication/WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public class Nokta
+        {
+            private Point p;
+            private Rectangle r;
+            private Color c;
+      
+            public Nokta(int x, int y)
+            {
+                p = new Point(x, y);
+                r = new Rectangle(x - 10 / 2, y - 10 / 2, 10, 10);
+                c = Color.Black;
+            }
+
+            public Point getPoint()
+            {
+                return p;
+            }
+
+            public Rectangle getRectangle()
+            {
+                return r;
+            }
+
+            public Color getColor()
+            {
+                return c;
+            }
+
+            public void setColor(Color col)
+            {
+                c = col;
+            }
+
+            public void setPoint(Point p)
+            {
+                this.p = p;
+                this.r = new Rectangle(p.X - 10 / 2, p.Y - 10 / 2, 10, 10);
+            }
+
+        }
+
+        List<Nokta> pointList = new List<Nokta>();
+        Boolean draw, translate, delete, translateStart, cluster;
+        Point[] clusterCentroids;
+        Point currentPoint;
+        int translatedPointIndex, clusterNumber;
+        Color translatedColor;
+        Color[] colors = { Color.Blue, Color.Chocolate, Color.Gold, Color.Green, Color.Orange, Color.Red };
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            draw = true;
+            translate = false;
+            delete = false;
+            translateStart = false;
+            cluster = false;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Rectangle r = new Rectangle(200, 100, 400,300);
+            g.DrawRectangle(Pens.Black, r);
+
+            if (!translateStart)
+                for (int i = 0; i < pointList.Count; i++)
+                    g.FillEllipse(new SolidBrush(pointList[i].getColor()), pointList[i].getRectangle());
+            else
+            {
+                for (int i = 0; i < pointList.Count; i++)
+                    g.FillEllipse(new SolidBrush(pointList[i].getColor()), pointList[i].getRectangle());
+
+                Rectangle rect = new Rectangle(currentPoint.X - 10 / 2, currentPoint.Y - 10 / 2, 10, 10);
+                g.FillEllipse(new SolidBrush(translatedColor), rect);
+            }
+
+            if (cluster)
+            {
+                for (int i = 0; i < clusterNumber; i++)
+                { 
+                    Point[] myPoints = {new Point(clusterCentroids[i].X,clusterCentroids[i].Y-5),
+                                        new Point(clusterCentroids[i].X+5,clusterCentroids[i].Y),
+                                        new Point(clusterCentroids[i].X,clusterCentroids[i].Y+5),
+                                        new Point(clusterCentroids[i].X-5,clusterCentroids[i].Y)};
+
+                    g.FillPolygon(Brushes.Gray, myPoints);
+                }
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Boolean found = false;
+
+            if (draw)
+            {
+                if ((e.X >= 200 && e.X <= 600) && (e.Y >= 100 && e.Y <= 400))//çizim alanında mı
+                {
+                    for (int i = 0; i < pointList.Count; i++)
+                    {
+                        if (pointList[i].getRectangle().Contains(e.X, e.Y))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        Nokta n = new Nokta(e.X, e.Y);
+                        pointList.Add(n);
+                        Invalidate();
+                    }
+                }
+            }
+
+            else if (delete)
+            {
+                if ((e.X >= 200 && e.X <= 600) && (e.Y >= 100 && e.Y <= 400))//çizim alanında mı
+                {
+                    for (int i = 0; i < pointList.Count; i++)
+                    { 
+                        if(pointList[i].getRectangle().Contains(e.X,e.Y))
+                        {
+                            pointList.RemoveAt(i);
+                            Invalidate();                          
+                        }
+                    }            
+                }
+            }   
+        }
+
+        private void addRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            draw = true;
+            translate = false;
+            delete = false;
+        }
+
+        private void translateRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            draw = false;
+            translate = true;
+            delete = false;
+        }
+
+        private void deleteRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            draw = false;
+            translate = false;
+            delete = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (translate)
+            {
+                for (int i = 0; i < pointList.Count; i++)
+                {
+                    if (pointList[i].getRectangle().Contains(e.X, e.Y))
+                    {
+                        currentPoint = new Point(e.X, e.Y);
+                        translatedPointIndex = i;
+                        translatedColor = pointList[i].getColor();
+                        pointList.RemoveAt(i);
+                        translateStart = true;
+                    }
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (translateStart)
+            {
+                currentPoint.X = e.X;
+                currentPoint.Y = e.Y;
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (translateStart)
+            {
+                Nokta n = new Nokta(e.X, e.Y);
+                n.setColor(translatedColor);
+                pointList.Add(n);
+                translateStart = false;
+                Invalidate(); 
+            }
+        }
+
+        public double euclideanDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt((Math.Pow(p1.X - p2.X, 2.0) + Math.Pow(p1.Y - p2.Y, 2.0)));
+        }
+
+        private void clusterButton_Click(object sender, EventArgs e)
+        {
+            int check = 0;
+            Random r = new Random();
+
+            clusterNumber = Convert.ToInt32(clusterNumberTextBox.Text);
+            clusterCentroids = new Point[clusterNumber];
+
+            for (int i = 0; i < clusterNumber; i++)
+            {
+                clusterCentroids[i].X = r.Next(200, 600);
+                clusterCentroids[i].Y = r.Next(100, 400);
+            }
+
+            cluster = true;
+            Invalidate();
+            double[,] distances = new double[clusterNumber, pointList.Count];
+       
+          
+            for (int k = 0; k < 20; k++)
+            {
+                for (int i = 0; i < clusterNumber; i++)
+                {
+                    for (int j = 0; j < pointList.Count; j++)
+                    {
+                        distances[i, j] = euclideanDistance(clusterCentroids[i], pointList[j].getPoint());
+                    }
+                }
+
+                for (int i = 0; i < pointList.Count; i++)
+                {
+                    double min = distances[0, i];
+                    int minCluster = 0;
+
+                    for (int j = 1; j < clusterNumber; j++)
+                    {
+                        if (distances[j, i] < min)
+                        {
+                            min = distances[j, i];
+                            minCluster = j;
+                            check = 1;
+                        }
+                    }
+                    pointList[i].setColor(colors[minCluster]);
+                }
+                Invalidate();
+
+                if (check == 0)
+                    break;    
+
+                for (int i = 0; i < clusterNumber; i++)
+                {
+                    int newX = 0;
+                    int newY = 0;
+                    int counter = 0;
+                    for (int j = 0; j < pointList.Count; j++)
+                    {
+                        if (pointList[j].getColor() == colors[i])
+                        {
+                            newX += pointList[j].getPoint().X;
+                            newY += pointList[j].getPoint().Y;
+                            counter++;
+                        }
+                    }
+                    clusterCentroids[i].X = newX / counter;
+                    clusterCentroids[i].Y = newY / counter;
+                }
+                  
+            }
+            Invalidate();
+
+
+        }
+
+    }
+}
